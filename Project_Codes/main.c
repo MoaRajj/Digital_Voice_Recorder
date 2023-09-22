1a6041cc14ea1e1d9c14c0782148adc199e9f249
Moayad Rajjoub <93192572+MoaRajj@users.noreply.github.com>
2023-09-22 16:23:39 +0300

    Update main.c

--- Project_Codes/main.c
+++ Project_Codes/main.c
@@ -1,9 +1,4 @@
 ////////////////////// main.c ///////////////////////
-/*
- * Moayad Rajjoub
- * 171024078
- *
- */
 
 #include "stm32g0xx.h"
 #include "project3.h"
@@ -12,19 +7,13 @@
 
 #define LEDDELAY 1600000U
 
-
 int main(void) {
-
-	BSP_System_init();
-	
-	init_adc() ;
-   	init_timer1();
-	init_I2C();
-    
-for(;;){
-
-	Keypad_enable();
-
+  BSP_System_init();
+  init_adc() ;
+  init_timer1();
+  init_I2C();
+  for(;;){
+    Keypad_enable();
   }
-    return 0;
+ return 0;
 }
